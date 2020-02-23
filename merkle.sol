44ee226103395753fd02e04d05023a64e8648346
sicalpath <kirishimasyouko@gmail.com>
2020-02-23 15:22:12 +0800

    relative-index

--- merkle.sol
+++ merkle.sol
@@ -43,10 +43,11 @@ contract Owned {
 
 contract MerkleTreeGenerator is Owned {
     
-    //using SafeMath for uint256;
+    using SafeMath for uint256;
     event Log(bytes data);
 
     CandyReceipt candyReceipt = CandyReceipt(0xb1DB8f1834ab5034142240f3bF615a3D703cBefa);
+    uint256 currentIndex = 0;
     bytes32[] public leafNodes;         //always empty
     //MerkleNode[] public merkleNodes;    //always empty
     MerkleTree[] public merkleTrees;
@@ -93,11 +94,14 @@ contract MerkleTreeGenerator is Owned {
         //clean the leafNodes
         delete leafNodes;
         
-        //fetch users' lock datas
-        uint256 leafCount = candyReceipt.receiptCount();
+        //fetch users' lock datas from currentIndex
+        //this leafCount include finished receipts
+        uint256 leafCount = candyReceipt.receiptCount().sub(currentIndex);
+        
+        assert(leafCount > 0);
         
         //Hash all leaves
-        for(uint256 i; i<leafCount; i++) {
+        for(uint256 i=0; i<leafCount; i++) {
             (
                 address asset,
                 address owner,
@@ -108,7 +112,7 @@ contract MerkleTreeGenerator is Owned {
                 uint256 startTime,
                 uint256 endTime,
                 bool finished 
-            ) = candyReceipt.receipts(i);
+            ) = candyReceipt.receipts(currentIndex + i);
             
             
             if(finished == false) {
@@ -118,6 +122,8 @@ contract MerkleTreeGenerator is Owned {
             
         }
         
+        currentIndex = candyReceipt.receiptCount();
+        
     }
     
     //create new receipt
@@ -126,7 +132,8 @@ contract MerkleTreeGenerator is Owned {
         //fetch data and generate leaves
         ReceiptsToLeaves();
         
-        uint256 leafCount = candyReceipt.receiptCount();
+        //this leafCount doesn't include finished receipts
+        uint256 leafCount = leafNodes.length;
         bytes32 left;
         bytes32 right;
         uint256 nodeToAdd = leafNodes.length / 2;
@@ -186,13 +193,24 @@ contract MerkleTreeGenerator is Owned {
     //get users merkle tree path
     function GenerateMerklePath(uint256 index) public view returns(bytes32[20],bool[20]) {
         
-        MerkleTree memory merkleTree = merkleTrees[merkleTrees.length - 1]; //先从最新的获取吧
+        assert(index < currentIndex);
+        //need to locate which MerkleTree include target index receipt
+        
+  
+        uint256 indices = 0;
+       
+        for(uint256 j=0 ;index >= indices ; j++){
+            indices = indices.add(merkleTrees[j].leaf_count);
+        }
+        index = index.sub(indices.sub(merkleTrees[j].leaf_count));      //convert to relative index
+
+        
+        MerkleTree memory merkleTree = merkleTrees[j]; 
         
         assert(index < merkleTree.leaf_count);
 
         bytes32[20] neighbors;
         bool[20] isLeftNeighbors;
-        
         uint256 indexOfFirstNodeInRow = 0;
         uint256 nodeCountInRow = merkleTree.leaf_count;
         bytes32 neighbor;
@@ -220,7 +238,6 @@ contract MerkleTreeGenerator is Owned {
             
             neighbors[i] = neighbor;
             isLeftNeighbors[i++] = isLeftNeighbor;
-            //merkleNodes[i++] = MerkleNode(neighbor, isLeftNeighbor);
             
             nodeCountInRow = nodeCountInRow % 2 == 0 ? nodeCountInRow : nodeCountInRow + 1;
             shift = (index - indexOfFirstNodeInRow) / 2;
@@ -237,4 +254,146 @@ contract MerkleTreeGenerator is Owned {
     }
     
     
+}
+
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     *
+     * _Available since v2.4.0._
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     *
+     * _Available since v2.4.0._
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts with custom message when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     *
+     * _Available since v2.4.0._
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
 }
